7d4fbad7aedd8549c550662f9dd1ae4ec7b6fa0a
mwaggett <mwaggett@alumni.reed.edu>
2015-09-18 12:59:54 -0700

    Write function to sing bottles of beer on the wall song starting with an input number; all specs met.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-// var functionName = function(params) {
-//
-// };
+var bottlesOfBeer = function(number) {
+  if (number === 0) {
+    return lastRound();
+  } else {
+    return oneRound(number) + bottlesOfBeer(number-1);
+  }
+}
+
+var oneRound = function(number) {
+  var nextNumber = number - 1;
+  return number + " bottles of beer on the wall, " + number + " bottles of beer. Take one down and pass it around, " + nextNumber + " bottles of beer on the wall. ";
+}
+
+var lastRound = function() {
+  return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+}
+
 
 // $(document).ready(function() {
 //
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,21 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('oneRound', function() {
+  it("returns the lyrics to one round of the 99 bottles of beer on the wall song for a specific number", function() {
+    expect(oneRound(99)).to.equal("99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall. ");
+  });
+});
+
+describe('lastRound', function() {
+  it("returns the last line of the 99 bottles of beer on the wall song", function() {
+    expect(lastRound()).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
+});
+
+describe('bottlesOfBeer', function() {
+  it("sings the whole 99 bottles of beer on the wall song starting at a given number", function() {
+    expect(bottlesOfBeer(4)).to.equal("4 bottles of beer on the wall, 4 bottles of beer. Take one down and pass it around, 3 bottles of beer on the wall. 3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall. 2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottles of beer on the wall. 1 bottles of beer on the wall, 1 bottles of beer. Take one down and pass it around, 0 bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
+
+  it("just sings last line if given 0", function() {
+    expect(bottlesOfBeer(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
+});
