153d26f3d8833a8c73d37768b808daad0baff7a3
mwaggett <mwaggett@alumni.reed.edu>
2015-09-18 13:58:05 -0700

    Fix lastRound function to have a startNumber parameter because I didn't realize it depended on that.. Now recursive bottlesOfBeer function is broken, but I don't really need it since adjusting web interface to display each line of the song as an item in a list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,31 @@
-var bottlesOfBeer = function(number) {
-  if (number === 0) {
-    return lastRound();
-  } else {
-    return oneRound(number) + bottlesOfBeer(number-1);
-  }
-}
+// var bottlesOfBeer = function(number) {
+//   if (number === 0) {
+//     return lastRound(number);
+//   } else {
+//     return oneRound(number) + bottlesOfBeer(number-1);
+//   }
+// }
+// I know this is broken for the last line. I just really wanted to use recursion. :[
 
 var oneRound = function(number) {
   var nextNumber = number - 1;
   return number + " bottles of beer on the wall, " + number + " bottles of beer. Take one down and pass it around, " + nextNumber + " bottles of beer on the wall. ";
 }
 
-var lastRound = function() {
-  return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+var lastRound = function(startNumber) {
+  return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, " + startNumber + " bottles of beer on the wall."
 }
 
 
 $(document).ready(function() {
   $("form#bottles").submit(function(event) {
     var number = parseInt($("input#number").val());
-    var song = bottlesOfBeer(number);
+    for (var bottle = number; bottle > 0; bottle--) {
+      $("#song").append("<li>"+oneRound(bottle)+"</li>");
+    }
+    $("#song").append("<li>"+lastRound(number)+"</li>");
 
     $("#input").text(number);
-    $("#song").text(song);
 
     $(".result").show();
     $(".input").hide();
--- spec/specs.js
+++ spec/specs.js
@@ -6,16 +6,12 @@ describe('oneRound', function() {
 
 describe('lastRound', function() {
   it("returns the last line of the 99 bottles of beer on the wall song", function() {
-    expect(lastRound()).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+    expect(lastRound(99)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
 });
 
-describe('bottlesOfBeer', function() {
-  it("sings the whole 99 bottles of beer on the wall song starting at a given number", function() {
-    expect(bottlesOfBeer(4)).to.equal("4 bottles of beer on the wall, 4 bottles of beer. Take one down and pass it around, 3 bottles of beer on the wall. 3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall. 2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottles of beer on the wall. 1 bottles of beer on the wall, 1 bottles of beer. Take one down and pass it around, 0 bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
-  });
-
-  it("just sings last line if given 0", function() {
-    expect(bottlesOfBeer(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
-  });
-});
+// describe('bottlesOfBeer', function() {
+//   it("sings the whole 99 bottles of beer on the wall song starting at a given number", function() {
+//     expect(bottlesOfBeer(4)).to.equal("4 bottles of beer on the wall, 4 bottles of beer. Take one down and pass it around, 3 bottles of beer on the wall. 3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall. 2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottles of beer on the wall. 1 bottles of beer on the wall, 1 bottles of beer. Take one down and pass it around, 0 bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 4 bottles of beer on the wall.");
+//   });
+// });
